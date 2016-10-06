067910597b8f084f4c7d8abee8d1c2522ee4837e
Amanda McCormack <amccormack@vcd-ch204-15.eastern.ewu.edu>
2016-10-06 14:17:05 -0700

    Added & Customized Plugin

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Test scripts.js /console.log('OK USA!');
+
+// Test jQuery /$('img').hide();
+
+$('img').imageLens({ 
+    imageSrc: "/img/watch-zoom.jpg",
+    lensSize: 400,
+    borderColor: 'yellow'
+    
+});
+
